c9cb0fe3a746fbb3e1bc68198ac300ce548c60f8
Aline Buchino <aline.buchino@hotmail.com>
2023-02-12 13:24:02 -0300

    criação da lista de categorias de jogos e estilização de componentes para finalização da home

--- src/components/Guild/styles.ts
+++ src/components/Guild/styles.ts
@@ -5,17 +5,21 @@ export const styles = StyleSheet.create({
   container: {
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 24,
-    marginTop: 20
+    alignItems: 'center',
+    paddingHorizontal: 24,    
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 20
   },
   title: {
     fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
     fontSize: 18,
-    marginLeft: -11
+    marginBottom: 4,
   },
-  subtitle: {
+  type: {
     fontFamily: theme.fonts.text400,
     color: theme.colors.highlight,
     fontSize: 13,
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { FlatList, View, Text } from "react-native";
+import { Appointment } from "../../components/Appointment";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
+import { ListDivider } from "../../components/ListDivider";
 import { ListHeader } from "../../components/ListHeader";
 import { Profile } from "../../components/Profile";
 import { styles } from "./styles";
@@ -14,6 +16,19 @@ export function Home() {
             id: '1',
             guild: {
                 id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+
+        {
+            id: '2',
+            guild: {
+                id: '2',
                 name: 'Lendários',
                 icon: null,
                 owner: true
@@ -51,8 +66,11 @@ export function Home() {
             data={appointments}
             keyExtractor={item => item.id}
             renderItem={({item}) => (
-                <Text>{item.guild.name}</Text>
+               <Appointment data={item} />
             )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
             />
 
             </View>
