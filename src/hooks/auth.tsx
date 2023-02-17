ffb58257723d9ab6c0d8c234dddc2fccd7276b94
Aline Buchino <aline.buchino@hotmail.com>
2023-02-17 15:59:44 -0300

    add AsyncStorage, api para armazenamento local

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,24 +1,24 @@
-/* centralização da parte do contexto */
-import React, {
+import React, 
+{ 
   createContext,
   useContext,
-  ReactNode,
   useState,
+  ReactNode,
   useEffect,
-} from "react";
+} from 'react';
 
-import * as AuthSession from "expo-auth-session";
-/* import AsyncStorage from "@react-native-async-storage/async-storage"; */
+import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
-const { RESPONSE_TYPE } = process.env;
 const { CLIENT_ID } = process.env;
 const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
 
-import { api } from "../services/apis";
-/* import { COLLECTION_APPOINTMENTS, COLLECTION_USERS } from "../configs/database";
- */
+import { api } from '../services/apis';
+import { COLLECTION_USERS } from '../configs/database';
+
 type User = {
   id: string;
   username: string;
@@ -26,25 +26,25 @@ type User = {
   avatar: string;
   email: string;
   token: string;
-};
+}
 
 type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
   singOut: () => Promise<void>;
-};
+}
 
 type AuthProviderProps = {
   children: ReactNode;
-};
+}
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
     access_token?: string;
     error?: string;
-  };
-};
+  }
+}
 
 export const AuthContext = createContext({} as AuthContextData);
 
@@ -56,75 +56,74 @@ function AuthProvider({ children }: AuthProviderProps) {
     try {
       setLoading(true);
 
-      /* URL para autenticação com o Discord */
-      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-      
-      /* criação da sessão */
-      const { type, params } = (await AuthSession.startAsync({
-        authUrl,
-      })) as AuthorizationResponse;
+      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`; 
 
-      /* 
-        só avança se a autenticação aconteceu 
-        e não deu erro (como não aceitar a autenticação) 
-      */
-      if (type === "success" && !params.error) {
-        /* captura dos dados do usuário e do token - pra identificar o 'usuário' (conseguir o response certo) */
+      const { type, params } = await AuthSession
+      .startAsync({ authUrl }) as AuthorizationResponse;
+
+      if(type === "success" && !params.error){ 
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
-        const userInfo = await api.get("/users/@me");
-        const firstName = userInfo.data.username.split(" ")[0];
+        const userInfo = await api.get('/users/@me');
+
+        const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
         const userData = {
           ...userInfo.data,
           firstName,
-          token: params.access_token,
-        };
-
-        /* await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
- */
-        setUser(userData);
+          token: params.access_token
+        }
+       
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+        setUser(userData);        
       }
     } catch {
-      throw new Error("Não foi possível autenticar");
+      throw new Error('Não foi possível autenticar');
     } finally {
-      /* independente se deu certo ou errado a autenticação, ele tira o loading */
       setLoading(false);
     }
   }
 
   async function singOut() {
     setUser({} as User);
-    /* await AsyncStorage.removeItem(COLLECTION_USERS); */
+    await AsyncStorage.removeItem(COLLECTION_USERS);    
   }
 
   async function loadUserStorageData() {
-    /* const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
 
-    if (storage) {
+    if(storage){
       const userLogged = JSON.parse(storage) as User;
       api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
 
       setUser(userLogged);
-    } */
+    }
   }
 
   useEffect(() => {
     loadUserStorageData();
-  }, []);
+  },[]);
 
   return (
-    <AuthContext.Provider value={{ user, loading, signIn, singOut }}>
-      {children}
+    <AuthContext.Provider value={{
+      user,
+      loading,
+      signIn,
+      singOut
+    }}>
+      { children }
     </AuthContext.Provider>
-  );
+  )
 }
 
 function useAuth() {
-  /* criação de hook próprio para autenticação */
   const context = useContext(AuthContext);
+
   return context;
 }
 
-export { AuthProvider, useAuth };
+export {
+  AuthProvider,
+  useAuth
+}
